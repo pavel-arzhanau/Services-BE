504c260529af9d662c1f2ea1adbe2fe0e8462a34
Pavel Arzhanau <pavelarzhanau@gmail.com>
2024-03-19 14:24:10 +0300

    init: connect to postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { RegionController } from './region.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'guansoo96',
+      database: 'services',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController, RegionController],
   providers: [AppService],
 })
--- src/region.controller.ts
+++ src/region.controller.ts
@@ -3,9 +3,16 @@ import { IRegion } from './dto';
 
 @Controller('region')
 export class RegionController {
-  
   @Get()
   getRegions(): Array<IRegion> {
-    return [{id: 1, name: 'Minsk' },{id: 2, name: 'Minsk Region' },{id: 3, name: 'Vitebsk Region' },{id: 4, name: 'Mogilev Region' },{id: 5, name: 'Grodno Region' }, {id: 5, name: 'Brest Region' }, {id: 5, name: 'Gomel Region' }]
+    return [
+      { id: 1, name: 'Minsk' },
+      { id: 2, name: 'Minsk Region' },
+      { id: 3, name: 'Vitebsk Region' },
+      { id: 4, name: 'Mogilev Region' },
+      { id: 5, name: 'Grodno Region' },
+      { id: 5, name: 'Brest Region' },
+      { id: 5, name: 'Gomel Region' },
+    ];
   }
 }
